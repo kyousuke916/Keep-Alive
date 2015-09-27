e9c8c901b4445ea70cb42e1a1194f07c75ed8dbc
septem1120 <septem.huang@gamesofa.com>
2015-09-27 09:54:20 +0800

    調整登入頁畫面

---
+++ Assets/Script/Menu/ButtonClick.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ButtonClick : MonoBehaviour {
+	
+	public void LoginLobby()
+	{
+		Application.LoadLevel ("Lobby");
+
+	}
+
+	public void LogOut()
+	{
+		GameObject gameObj = GameObject.Find ("LobbyManager");
+		if (gameObj) 
+		{
+			gameObj.SetActive(false);
+		}
+
+		Application.LoadLevel ("Menu");
+	}
+}
